3ec86c193d2aa621f70acf893e3bbd1eb5c8ae87
binarytraderch-sys <binarytraderch@gmail.com>
2026-05-25 11:10:07 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from '@/components/ui/sonner';
 import { routes } from './routes';
+
+function App() {
+  return (
+    <Router>
+      <Toaster />
+      <Routes>
+        {routes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={route.element}
+          />
+        ))}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
